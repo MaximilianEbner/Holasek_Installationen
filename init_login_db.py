d808ed3bb0f969ed0a7340330755429a0491a51c
MaximilianEbner <max.ebner@hotmail.com>
2025-07-22 14:20:36 +0200

    Delete init_login_db.py

--- init_login_db.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-
-# Initialisiere die Datenbank mit allen Tabellen
-with app.app_context():
-    db.create_all()
-    print("Datenbank mit LoginAdmin-Tabelle initialisiert!")
